9ae94e0622d91bb0ed1ed109f836250ec49557b7
Mark Johnson <lfmark7@gmail.com>
2017-02-02 02:29:58 +0000

    final repair

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  resources :microposts
-  resources :users
+  get 'static_pages/home'
+
+    get 'static_pages/about'
+
+  get 'static_pages/help'
+
+  resources :users 
+      
+  resources :micropost
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root 'users#index'
-
 end
+
